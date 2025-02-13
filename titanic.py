497fd0e029be74a4fcc9675cd4f277b1dc0bba0d
akira-kitajima-p <progressive@live.jp>
2025-02-13 23:31:13 +0900

    学習方法の改善、検証の追加
    ・全ての学習方法/特徴量の組み合わせで学習できるように
    ・10回の提出制限を超えたので100%データを取得してローカルで精度を検証出来るように

--- titanic.py
+++ titanic.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from titanic_pretreatment import tutrial3
 from predict import drop_train_predict
+from sklearn.metrics import accuracy_score
 
 train = pd.read_csv('./train.csv')
 test = pd.read_csv('./test.csv')
@@ -22,17 +23,27 @@ X_test = test.drop('Survived', axis=1)
 
 # 予測パターンと出力ファイル名を定義
 drop_patterns = {
-    # "submission_familySize_isAlone.csv": [],
-    # "submission_isAlone.csv": ["FamilySize"],
-    # "submission_FamilySize.csv": ["IsAlone"],
+    "submission_familySize_isAlone.csv": [],
+    "submission_isAlone.csv": ["FamilySize"],
+    "submission_FamilySize.csv": ["IsAlone"],
     "submission.csv": ["FamilySize", "IsAlone"]
 }
 
+# ローカル検証用にタイタニックの100%データを準備
+valid = pd.read_csv('./titanic-valid-submission.csv')
+
 # 使いたいモデルを変更可能（"lr", "rf", "lgbm"）
-model_type = "rf"
+model_types = ["lr", "rf", "lgbm"]
 
 # ループで学習と予測を実行
 sub = gender_submission
 for filename, drops in drop_patterns.items():
-    sub['Survived'] = drop_train_predict(X_train, y_train, X_test, model_type, drops)
-    sub.to_csv(filename.replace("submission", f"{model_type}_submission"), index=False)
+    for model_type in model_types:
+        sub['Survived'] = drop_train_predict(X_train, y_train, X_test, model_type, drops)
+        label = filename.replace("submission", f"{model_type}_submission")
+        accuracy = accuracy_score(valid['Survived'], sub['Survived'])
+        print(label + " " + str(accuracy))
+        sub.to_csv(label, index=False)
+    
+
+    
